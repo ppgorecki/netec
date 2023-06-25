56a19de20b616972fba9ebeec27ab17d9e3e7c9a
talkana <nataliaruteckaa@gmail.com>
2023-06-25 12:02:32 +0200

    Add simulations for masking thresholds 0.8-1

--- preprocessing_scripts/mask_taxa.py
+++ preprocessing_scripts/mask_taxa.py
@@ -10,14 +10,15 @@ def clear_taxa(g: Tree, p: float) -> Tree:
             leaf.label = "?"
     return g
 
-thresholds = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7]
+
+thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 repeats = 10
-tree_files = ["../data_yeast/gtrees"] + [ f"../data_sim/wgd-{i}-gene-trees" for i in [1,2,3,4,5] ]  
+tree_files = ["../data_yeast/gtrees"] + [f"../data_sim/wgd-{i}-gene-trees" for i in [1, 2, 3, 4, 5]]
 for threshold in thresholds:
     for i in range(repeats):
-        for tree_file in tree_files: 
+        for tree_file in tree_files:
             fn = f"{tree_file}_{threshold}_{i + 1}"
-            if os.path.exists(fn): 
+            if os.path.exists(fn):
                 print(f"File {fn} exists (skip)")
                 continue
             with open(fn, "w") as out:
